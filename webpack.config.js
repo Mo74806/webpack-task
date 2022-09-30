041353538ee6d7f60d089d94b609685a0b484a40
Mo74806 <M.khaled201774@gmail.com>
2022-09-30 23:01:00 +0200

    first

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const pathModule = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "script.min.js",
+    path: pathModule.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin(), new MiniCssExtractPlugin()],
+  optimization: {
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["mozjpeg", { quality: 60 }],
+              ["optipng", { optimizationLevel: 5 }],
+              [
+                "svgo",
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      convertShapeToPath: {
+                        convertArcs: true,
+                      },
+                      convertPathData: false,
+                    },
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+};
